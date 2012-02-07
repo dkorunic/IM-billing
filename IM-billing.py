4f43daa3dfbd26f1c0d0edf29a529c88e1205051
Dinko Korunic <dinko.korunic@gmail.com>
2012-02-07 13:06:28 +0100

    - finally fix local tz to GMT conversions...

--- IM-billing.py
+++ IM-billing.py
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 """
 
-__version__ = '$Id: IM-billing.py,v ae84f692f79e 2012/02/07 11:02:40 dinko $'
+__version__ = '$Id: IM-billing.py,v d1a1dab55f83 2012/02/07 12:06:28 dinko $'
 
 import getopt
 import sys
@@ -80,32 +80,31 @@ class IMBilling:
                 'private', 'full')
         query.max_results = 10000
 
-        # prepare start/end dates
-        time_format = '%Y-%m-%d'
+        # prepare start/end dates and do the neccessary fugly conversions
+        time_format_output = '%Y-%m-%dT%H:%M:%S.000Z'
+        time_format_input = '%Y-%m-%d'
         if start_min is None:
             # return work in last 60 days
-            query.start_min = time.strftime(time_format,
+            query.start_min = time.strftime(time_format_output,
                     time.gmtime(time.time() - 86400 * 60))
         else:
-            query.start_min = start_min
+            tuple_time = time.gmtime(time.mktime(time.strptime(start_min,
+                time_format_input)))
+            query.start_min = time.strftime(time_format_output,
+                    tuple_time)
         if start_max is None:
             # end time is now!
-            query.start_max = time.strftime(time_format,
+            query.start_max = time.strftime(time_format_output,
                     time.gmtime(time.time()))
         else:
-            query.start_max = start_max
-
-        # fugly fixups
-        query.start_min = '%sT00:00:00' % query.start_min
-        query.start_max = '%sT23:59:59' % query.start_max
+            tuple_time = time.gmtime(time.mktime(time.strptime(start_max,
+                time_format_input)))
+            query.start_max = time.strftime(time_format_output,
+                    tuple_time)
 
         # print headers
         print 'Listing work done on %s project from %s to %s' % \
                 (calendar, query.start_min, query.start_max)
-
-        # implement basic sanity bound checking
-        real_start_max = dateutil.parser.parse(query.start_max).replace(tzinfo=dateutil.tz.tzlocal())
-        real_start_min = dateutil.parser.parse(query.start_min).replace(tzinfo=dateutil.tz.tzlocal())
 
         # execute the query
         feed = self.calendar_service.CalendarQuery(query)
@@ -122,11 +121,6 @@ class IMBilling:
                 # ISO8601 parsing might not work with Python3
                 start_date = dateutil.parser.parse(a_when.start_time)
                 end_date = dateutil.parser.parse(a_when.end_time)
-
-                if start_date < real_start_min or start_date > real_start_max:
-                    print 'Oops, got date out of bounds! Start:', \
-                        start_date, 'End:', end_date
-                    continue
 
                 # time calculations
                 current_date = workday_output_format % (start_date.year,
@@ -174,6 +168,7 @@ def usage():
     print 'python IM-billing.py --user username --pw password ' \
             '--calendar calendar_name [ --start YYYY-MM-DD ] ' \
             '[ --end YYYY-MM-DD ] [ --rate rate_per_hour ]'
+    print 'Please note that --end is exclusive, while --start is inclusive.'
     sys.exit(2)
 
 def main():
